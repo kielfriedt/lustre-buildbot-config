08344000b2dd4b7a7c0fd9dc4bf924b593241da3
kiel friedt <friedt2@llnl.gov>
2017-03-15 13:01:34 -0700

    added more props

--- scripts/bb-bootstrap.sh
+++ scripts/bb-bootstrap.sh
@@ -57,6 +57,7 @@ fi
 
 if test ! -f /etc/buildslave; then
     echo "SPACK_URL=\"$SPACK_URL\""      > /etc/buildslave
+    echo "XSDK_URL=\"$XSDK_URL\""       >> /etc/buildslave
     echo "BB_MASTER=\"$BB_MASTER\""     >> /etc/buildslave
     echo "BB_NAME=\"$BB_NAME\""         >> /etc/buildslave
     echo "BB_PASSWORD=\"$BB_PASSWORD\"" >> /etc/buildslave
